cccbb7207a099ce5984c4eb02ff05da480f89cb9
Adrian Torres <adrianluistorres2@gmail.com>
2023-01-06 11:15:20 -0500

    subiendo proyecto al repositorio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UserForm from './componentes/UserForm';
+import DatosUsuario from './componentes/DatosUsuario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
